dc82e0a99cdaa94dbd57110806398b0c040e8279
christopherDallar <christopherdallar1234@gmail.com>
2024-01-21 02:21:07 -0400

    W02: Programming Task

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,36 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = "Christopher Gómez Dallar";
+let currentYear = new Date().getFullYear();
+let profilePicture = "images/my_picture.jpeg";
 
 /* Step 3 - Element Variables */
 
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const pictureElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
 
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+pictureElement.src = profilePicture;
+pictureElement.alt = ` Profile image of ${fullName}`;
 
 /* Step 5 - Array */
+const favoriteFoods = ["lasagna", "hamburger", "pasta bologna", "brownie"];
+const singleFood = "pizza";
 
+favoriteFoods.push(singleFood);
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
+favoriteFoods.shift();
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
+favoriteFoods.pop();
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
